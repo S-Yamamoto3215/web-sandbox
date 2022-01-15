08445da1833793e19641ec2f51e971d3dd4de4cd
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-01-15 18:17:34 +0900

    :sparkles: FIX Function checkLength
    Function to check for username and password activation.
    least 3 characters for username,least 6 characters for password.
    
    [optional Reference URL]

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -7,10 +7,34 @@ const checkRequired = () => {
   const inputPassword = <HTMLInputElement>document.getElementById('password');
   const inputPassword2 = <HTMLInputElement>document.getElementById('password2');
   const result = {
-    name: inputName,
-    email: inputEmail,
-    password: inputPassword,
-    password2: inputPassword2,
+    inputName: inputName,
+    inputEmail: inputEmail,
+    inputPassword: inputPassword,
+    inputPassword2: inputPassword2,
+  }
+
+  return result
+}
+
+const checkLength = (targetElement: HTMLInputElement) => {
+  const [value, id] = [targetElement.value, targetElement.id];
+  const result = {
+    isValidation: true,
+    errorMessage: ""
+  }
+
+  switch (id) {
+    case "name":
+      if (value.length < 3) {
+        result.isValidation = false;
+        result.errorMessage = "Username must be at least 3 characters";
+      }
+      break;
+    case "password":
+      if (value.length < 6) {
+        result.isValidation = false;
+        result.errorMessage = "Password must be at least 6 characters";
+      }
   }
 
   return result
@@ -20,5 +44,7 @@ const checkRequired = () => {
 form.addEventListener('submit', event => {
   event.preventDefault();
 
-  const { name, email, password, password2 } = checkRequired();
+  const { inputName, inputEmail, inputPassword, inputPassword2 } = checkRequired();
+  // checkEmail()
+  // checkPasswordsMatch()
 })
