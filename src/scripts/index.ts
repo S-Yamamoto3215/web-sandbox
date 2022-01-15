a31c0728bbe46c47c8fc997b4405d271fe0e4b0f
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-01-15 17:15:48 +0900

    :sparkles: FIX function checkRequired
    Function to return the input element of a form.
    Created for retrieving values and assigning classes.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,24 @@
 // get form Element
 const form = document.getElementById('form');
 
+const checkRequired = () => {
+  const inputName = <HTMLInputElement>document.getElementById('name');
+  const inputEmail = <HTMLInputElement>document.getElementById('email');
+  const inputPassword = <HTMLInputElement>document.getElementById('password');
+  const inputPassword2 = <HTMLInputElement>document.getElementById('password2');
+  const result = {
+    name: inputName,
+    email: inputEmail,
+    password: inputPassword,
+    password2: inputPassword2,
+  }
+
+  return result
+}
+
 // form submit
 form.addEventListener('submit', event => {
   event.preventDefault();
-  console.log("submit!!!")
+
+  const { name, email, password, password2 } = checkRequired();
 })
