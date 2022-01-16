f6fdd4b9e79c4336ab04e81e4761fda375279d3a
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-01-16 13:55:14 +0900

    :sparkles: FIX function switchingView
    Give class to input according to the activation.
    Add css to change colors based on class.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -7,6 +7,22 @@ const form = document.getElementById('form');
 form.addEventListener('submit', event => {
   event.preventDefault();
 
-  const inputElements = checkRequired();
-  console.log(inputElements)
+  const { inputName, inputEmail, inputPassword, inputPassword2 } = checkRequired();
+
+  switchingView(checkLength(inputName), inputName);
+  switchingView(checkEmail(inputEmail), inputEmail);
+  switchingView(checkLength(inputPassword), inputPassword);
+  switchingView(checkPasswordsMatch(inputPassword, inputPassword2), inputPassword2);
 })
+
+
+const switchingView = (result: {isValidation: boolean, errorMessage: string}, inputElement: HTMLInputElement) => {
+  const { isValidation, errorMessage} = result;
+  if (isValidation) {
+    inputElement.classList.add('form-input-success')
+    inputElement.classList.remove('form-input-error')
+  } else {
+    inputElement.classList.add('form-input-error')
+    inputElement.classList.remove('form-input-success')
+  }
+}
