e1f212b4ba00f92ef96d668342ce3a8db6b823b0
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-01-16 13:02:39 +0900

    :recycle: Refactor a set of functions into a separate file
    Because the outlook of index.ts became worse.
    Export functions together in functions.ts

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,83 +1,12 @@
+import { checkRequired, checkLength, checkEmail, checkPasswordsMatch } from "./functions";
+
 // get form Element
 const form = document.getElementById('form');
-
-const checkRequired = () => {
-  const inputName = <HTMLInputElement>document.getElementById('name');
-  const inputEmail = <HTMLInputElement>document.getElementById('email');
-  const inputPassword = <HTMLInputElement>document.getElementById('password');
-  const inputPassword2 = <HTMLInputElement>document.getElementById('password2');
-  const result = {
-    inputName: inputName,
-    inputEmail: inputEmail,
-    inputPassword: inputPassword,
-    inputPassword2: inputPassword2,
-  }
-
-  return result
-}
-
-const checkLength = (targetElement: HTMLInputElement) => {
-  const [value, id] = [targetElement.value, targetElement.id];
-  const result = {
-    isValidation: true,
-    errorMessage: ""
-  }
-
-  switch (id) {
-    case "name":
-      if (value.length < 3) {
-        result.isValidation = false;
-        result.errorMessage = "Username must be at least 3 characters";
-      }
-      break;
-    case "password":
-      if (value.length < 6) {
-        result.isValidation = false;
-        result.errorMessage = "Password must be at least 6 characters";
-      }
-  }
-
-  return result
-}
-
-const checkEmail = (targetElement: HTMLInputElement) => {
-  const result = {
-    isValidation: true,
-    errorMessage: ""
-  }
-
-  let emailReg = new RegExp('^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}.[A-Za-z0-9]{1,}$');
-  const value = targetElement.value;
-
-  if (!emailReg.test(value)) {
-    result.isValidation = false;
-    result.errorMessage = "Email is not valid";
-  }
-
-  return result
-}
-
-const checkPasswordsMatch = (passwordElement: HTMLInputElement, password2Element: HTMLInputElement) => {
-  const result = {
-    isValidation: true,
-    errorMessage: ""
-  }
-
-  const passwordValue = passwordElement.value
-  const password2Value = password2Element.value
-  const isMatch = passwordValue === password2Value
-
-  if (!isMatch) {
-    result.isValidation = false;
-    result.errorMessage = !password2Value ? "Password2 is required" : "Email is not valid"
-  }
-
-  return result
-}
 
 // form submit
 form.addEventListener('submit', event => {
   event.preventDefault();
 
-  const { inputName, inputEmail, inputPassword, inputPassword2 } = checkRequired();
+  const inputElements = checkRequired();
+  console.log(inputElements)
 })
