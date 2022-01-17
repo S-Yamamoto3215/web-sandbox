d942efee37de03c2aee50414f777fc04cd5c9c7d
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-01-17 23:31:59 +0900

    :sparkles: get seats, and click handler
    Implementing the firing of events when a sheet is clicked.
    Click to toggle Selected on and off.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, TypeScript")
+const seats = document.getElementById('seats').getElementsByClassName('seat')
+
+const seatClickHandler = (seat: Element) => {
+  seat.classList.toggle("selected");
+}
+
+for (let i = 0; i < seats.length; i++) {
+  const seat = seats[i];
+  const isOccupied = seat.classList.contains("occupied");
+  if (!isOccupied) {
+    seat.addEventListener('click', () => seatClickHandler(seat))
+  }
+}
