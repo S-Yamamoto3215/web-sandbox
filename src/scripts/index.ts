63b35eedf3459ed0672bc80116b60712d2f9a0c7
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-01-18 17:46:35 +0900

    :sparkles: click and selec event handling

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,13 +1,37 @@
+// Seet Event
 const seats = document.getElementById('seats').getElementsByClassName('seat')
 
 const seatClickHandler = (seat: Element) => {
   seat.classList.toggle("selected");
+  const { selectedSeatCount, movePrice } = getCurrentData();
+  console.log(selectedSeatCount, movePrice)
 }
 
 for (let i = 0; i < seats.length; i++) {
   const seat = seats[i];
   const isOccupied = seat.classList.contains("occupied");
   if (!isOccupied) {
-    seat.addEventListener('click', () => seatClickHandler(seat))
+    seat.addEventListener('click', () => seatClickHandler(seat));
   }
+}
+
+// Pick Event
+const moves = document.getElementById('moves');
+
+const moveSelectHandler = () => {
+  const { selectedSeatCount, movePrice } = getCurrentData();
+  console.log(selectedSeatCount, movePrice)
+}
+
+moves.addEventListener('change', () => moveSelectHandler())
+
+// get current data
+const getCurrentData = () => {
+  const currentSelectedSeats = document.getElementById('seats').getElementsByClassName('selected');
+  const selectedSeatCount = currentSelectedSeats.length;
+
+  const currentSelectedMove = <HTMLInputElement>document.getElementById('moves');
+  const movePrice = Number(currentSelectedMove.value)
+
+  return { selectedSeatCount: selectedSeatCount, movePrice: movePrice }
 }
